409b5f1063c45cdd53f59aa4d930fbf3ddc44100
kholish <kholish.nur8306@gmail.com>
2026-05-09 14:00:25 +0700

    add breeze

--- pitstop/routes/web.php
+++ pitstop/routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\LayananController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::post('/dashboard/bookings', [DashboardController::class, 'store'])->name('dashboard.bookings.store');
-Route::resource('/layanan', LayananController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::view('/tentang', 'tentang');
-Route::view('/kontak', 'kontak')->name('kontak');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
